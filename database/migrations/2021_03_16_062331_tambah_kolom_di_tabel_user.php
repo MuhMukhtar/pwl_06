d42a54b2806239312cae64ed8a7a14da1577f913
MuhMukhtar <mukhtar55217@gmail.com>
2021-03-16 13:27:53 +0700

    edit file di migration

--- database/migrations/2021_03_16_062331_tambah_kolom_di_tabel_user.php
+++ database/migrations/2021_03_16_062331_tambah_kolom_di_tabel_user.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Schema;
 
 class TambahKolomDiTabelUser extends Migration
@@ -13,7 +14,9 @@ class TambahKolomDiTabelUser extends Migration
      */
     public function up()
     {
-        //
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('username', 20)->after('id')->nullable()->unique();
+        });
     }
 
     /**
@@ -23,6 +26,8 @@ class TambahKolomDiTabelUser extends Migration
      */
     public function down()
     {
-        //
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('username');
+        });
     }
 }
